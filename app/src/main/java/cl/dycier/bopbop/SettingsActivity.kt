16ec34147fd813811dd814c713b249c1249d7ac7
Diego <dcastro20@alumnos.utalca.cl>
2023-09-16 23:03:16 -0300

    Ajustes menores en pantalla Settings y MainActivity

--- app/src/main/java/cl/dycier/bopbop/SettingsActivity.kt
+++ app/src/main/java/cl/dycier/bopbop/SettingsActivity.kt
@@ -3,12 +3,26 @@ package cl.dycier.bopbop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.preference.PreferenceFragment
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsActivity : PreferenceActivity() {
+
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-
-        addPreferencesFromResource(R.xml.preferencias)
+        setContentView(R.layout.activity_settings);
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.configuracion, PreferenciasFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+    class PreferenciasFragment : PreferenceFragmentCompat(){
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.preferencias, rootKey)
+        }
+    }
+
 }
