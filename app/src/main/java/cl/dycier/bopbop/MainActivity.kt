46fbed456508f975540f16441f418b4226f9e585
Diego <dcastro20@alumnos.utalca.cl>
2023-09-16 21:15:03 -0300

    Creación y edición de pantalla Settings, creación de las preferencias y ajustes menores

--- app/src/main/java/cl/dycier/bopbop/MainActivity.kt
+++ app/src/main/java/cl/dycier/bopbop/MainActivity.kt
@@ -1,6 +1,7 @@
 package cl.dycier.bopbop
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val intentAbout = Intent(this, AboutActivity::class.java)
             startActivity(intentAbout)
             }
+
+        val buttonSettings = findViewById<Button>(R.id.settingbutton)
+
+        buttonSettings.setOnClickListener {
+            val intentSettings = Intent(this, SettingsActivity::class.java)
+            startActivity(intentSettings)
+        }
 
     }
 }
--- app/src/main/java/cl/dycier/bopbop/SettingsActivity.kt
+++ app/src/main/java/cl/dycier/bopbop/SettingsActivity.kt
@@ -2,10 +2,13 @@ package cl.dycier.bopbop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : PreferenceActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        addPreferencesFromResource(R.xml.preferencias)
     }
 }
