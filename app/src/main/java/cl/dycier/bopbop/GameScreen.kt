4540e0148ee6d09aee235ee551e1059f3591fbc8
Diego Castro Ruiz <dcastro20@alumnos.utalca.cl>
2023-10-26 10:48:24 -0300

    Creación de la acción de deslizar (onFling) y doble click (onDoubleTap)

--- app/src/main/java/cl/dycier/bopbop/GameScreen.kt
+++ app/src/main/java/cl/dycier/bopbop/GameScreen.kt
@@ -5,7 +5,11 @@ import android.media.MediaPlayer
 import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
 import android.widget.Button
+import android.widget.Toast
 
 class GameScreen : AppCompatActivity() {
 
@@ -13,6 +17,8 @@ class GameScreen : AppCompatActivity() {
     private lateinit var mediaLose: MediaPlayer
     private var playbackParams: PlaybackParams? = null
     private var playbackParams2: PlaybackParams? = null
+
+    private lateinit var gestureManager: GestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,8 @@ class GameScreen : AppCompatActivity() {
 
         mediaLose = MediaPlayer.create(this, R.raw.losesound)
         playbackParams2 = mediaLose.playbackParams
+
+        gestureManager = GestureDetector(this, GestoEscucha())
 
 
         val buttonVictory = findViewById<Button>(R.id.victoryButton)
@@ -36,7 +44,34 @@ class GameScreen : AppCompatActivity() {
             mediaLose.start()
         }
 
+    }
 
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gestureManager.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
 
+    inner class GestoEscucha : SimpleOnGestureListener() {
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            showToast("Deslizado Exitoso")
+            return true
+        }
+
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            showToast2("Doble Click")
+            return true
+        }
+    }
+
+    private fun showToast(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+    }
+    private fun showToast2(mensajeDC : String){
+        Toast.makeText(this, mensajeDC, Toast.LENGTH_SHORT).show()
     }
 }
