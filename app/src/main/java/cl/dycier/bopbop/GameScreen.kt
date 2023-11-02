4503ac71ae78cbf86fa41cf80b33b7820db92e94
Diego Castro Ruiz <dcastro20@alumnos.utalca.cl>
2023-11-02 10:34:12 -0300

    Implementación de acelerómetro y random, generación de instrucciones cada 3 segundos

--- app/src/main/java/cl/dycier/bopbop/GameScreen.kt
+++ app/src/main/java/cl/dycier/bopbop/GameScreen.kt
@@ -1,24 +1,46 @@
 package cl.dycier.bopbop
 
-import android.content.Intent
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
-class GameScreen : AppCompatActivity() {
+class GameScreen : AppCompatActivity(), SensorEventListener {
 
     private lateinit var mediaVictory: MediaPlayer
     private lateinit var mediaLose: MediaPlayer
     private var playbackParams: PlaybackParams? = null
     private var playbackParams2: PlaybackParams? = null
 
+    private var sensorManager: SensorManager? = null
+    private var accelerometro: Sensor? = null
+
+    private lateinit var textInstrucion: TextView
+    private val handler = Handler(Looper.getMainLooper())
+
     private lateinit var gestureManager: GestureDetector
+
+    val randomValues = List(10) { Random.nextInt(0, 3) }
+    private val instructions = listOf(
+        "Desliza por la pantalla.",
+        "Agita el dispositivo.",
+        "Realiza dos toques."
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +53,15 @@ class GameScreen : AppCompatActivity() {
         playbackParams2 = mediaLose.playbackParams
 
         gestureManager = GestureDetector(this, GestoEscucha())
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometro = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        textInstrucion = findViewById(R.id.instruccion)
+        showRandomInstruction()
+
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
 
         val buttonVictory = findViewById<Button>(R.id.victoryButton)
@@ -51,6 +82,17 @@ class GameScreen : AppCompatActivity() {
         return super.onTouchEvent(event)
     }
 
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(this, accelerometro, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager?.unregisterListener(this)
+    }
+
+
     inner class GestoEscucha : SimpleOnGestureListener() {
         override fun onFling(
             e1: MotionEvent,
@@ -59,6 +101,7 @@ class GameScreen : AppCompatActivity() {
             velocityY: Float
         ): Boolean {
             showToast("Deslizado Exitoso")
+            //textInstrucion.text = "SE DESLIZO"
             return true
         }
 
@@ -73,5 +116,45 @@ class GameScreen : AppCompatActivity() {
     }
     private fun showToast2(mensajeDC : String){
         Toast.makeText(this, mensajeDC, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            val acceleration = Math.sqrt(x * x + y * y + z * z.toDouble()).toFloat()
+
+            // You can adjust the acceleration threshold based on your needs
+            val threshold = 10.0f
+
+            if(acceleration > threshold)
+            {
+                if(textInstrucion.text == "Agita el dispositivo.")
+                {
+                    //textInstrucion.text = "Wena mi rey"
+                }else if(textInstrucion.text != "Wena mi rey")
+                {
+                    textInstrucion.text = "El loco bruto"
+                    scheduleRandomInstruction()
+                }
+                showToast("Agitado")
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    private fun showRandomInstruction() {
+        val randomIndex = (0 until instructions.size).random() // Seleccionar índice aleatorio
+        val randomInstruction = instructions[randomIndex] // Obtener la instrucción aleatoria
+        textInstrucion.text = randomInstruction // Mostrar la instrucción en el TextView
+    }
+
+    private fun scheduleRandomInstruction() {
+        handler.postDelayed({
+            showRandomInstruction() // Realizar la función después de 3 segundos
+        }, 3000) // Retraso de 3 segundos (3000 milisegundos)
     }
 }
